c0b1ac7563f564adaa00d7aa6b3d03fb2e8f7b82
CHODANIEL <eksldpf2299@naver.com>
2025-08-27 11:28:49 +0900

    1

---
+++ frontend/src/components/home/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Nav from "./Nav"
+import "../style/Header.scss"
+
+const Header = () => {
+  return (
+    <header>
+      <div className="inner">
+        <h4>LOGO</h4>
+        <div className="right-wrap">
+          <Nav />
+          <button>DARK</button>
+        </div>
+      </div>
+
+    </header>
+  )
+}
+
+export default Header
